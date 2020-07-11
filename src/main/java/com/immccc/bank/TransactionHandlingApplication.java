965bcd6f7599c6feb3048fe3e1b2ab4c09798571
ïmmccc <immccc@gmail.com>
2020-07-11 12:12:59 +0200

    Initial

---
+++ src/main/java/com/immccc/bank/TransactionHandlingApplication.java
@@ -0,0 +1,13 @@
+package com.immccc.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionHandlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionHandlingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/immccc/bank/TransactionHandlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.immccc.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionHandlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
