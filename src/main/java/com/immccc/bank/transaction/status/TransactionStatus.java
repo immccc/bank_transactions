152bbb3342775d16437e33643f0b73cef066fe3c
ïmmccc <immccc@gmail.com>
2020-07-12 02:01:24 +0200

    Avoid null fees to be shown in transaction status

--- src/main/java/com/immccc/bank/transaction/status/TransactionStatus.java
+++ src/main/java/com/immccc/bank/transaction/status/TransactionStatus.java
@@ -1,11 +1,14 @@
 package com.immccc.bank.transaction.status;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
 import lombok.Builder;
 import lombok.Value;
 
 import java.math.BigDecimal;
+
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
 
 @Value
 @Builder(builderClassName = "TransactionStatusBuilder", toBuilder = true)
@@ -18,6 +21,8 @@ class TransactionStatus {
     String reference;
     Status status;
     BigDecimal amount;
+
+    @JsonInclude(NON_NULL)
     BigDecimal fee;
 
 }
